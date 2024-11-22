d72e0d6c30f89c8160eb5b35d9098405ba0e4631
Clément Murzeau <clement@studiorelief.fr>
2024-11-22 18:06:41 +0100

    first deploy

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,84 @@
-import { greetUser } from '$utils/greet';
+import './index.css';
+
+import barba from '@barba/core';
+import { restartWebflow } from '@finsweet/ts-utils';
+import gsap from 'gsap';
+
+// import gsap from 'gsap';
+import { darkMode } from '$utils/global/darkmode';
+import {
+  /* gsapTransitionIn, */
+  gsapTransitionOut,
+  initBarbaClick,
+} from '$utils/global/gsap-transition';
+import { loadModelViewerScript } from '$utils/global/modalviewer';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  /* first load */
+  initBarbaClick();
+  loadModelViewerScript();
+
+  /* Global */
+  darkMode(
+    // variables
+    `
+    text-color--text-primary,
+    text-color--text-alternate,
+    background-color--background-primary,
+    background-color--background-alternate,
+    background-color--background-grey,
+    border-color--border-primary,
+    border-color--border-alternate
+    `.replace(/\s+/g, ''),
+    // durée
+    0.2,
+    // ease
+    'power1.inOut'
+  );
+
+  barba.init({
+    transitions: [
+      {
+        name: 'page-transition',
+        async leave(data) {
+          await gsapTransitionOut();
+          gsap.to(data.current.container, { opacity: 0, duration: 0.5 });
+        },
+        async enter(data) {
+          await gsap.to(data.next.container, { opacity: 1, duration: 1 });
+        },
+      },
+    ],
+  });
+
+  barba.hooks.beforeEnter((data) => {
+    console.log('hook beforeEnter');
+
+    /* 
+    TODO: À retravailler -> Comprendre & régler async / await x Barba & GSAP
+    TODO: Check Flip plugin GSAP
+    */
+    const AnimationPageText = () => {
+      const dataNamespace = data.next.namespace;
+      console.log(dataNamespace);
+      const transitionText = document.querySelector('.transition_text');
+      if (transitionText) {
+        transitionText.textContent = dataNamespace;
+      }
+    };
+    AnimationPageText();
+  });
+
+  barba.hooks.after((data) => {
+    console.log('hook After');
+    console.log(data);
+
+    restartWebflow();
+    initBarbaClick();
+  });
 });
+
+//
+// window.Webflow.push(() => {
+// });
---
+++ src/utils/global/gsap-transition.ts
@@ -0,0 +1,82 @@
+import gsap from 'gsap';
+
+export const gsapTransitionOut = () => {
+  // Selector
+  const logo = document.querySelector('.navbar_logo-round');
+  const main = document.querySelector('.main-wrapper');
+  const transitionWrapper = document.querySelector('.transition_component');
+  const transitionText = document.querySelector('.transition_text');
+
+  // Variables
+  const viewportHeight = window.innerHeight;
+  const scaleAmount = viewportHeight;
+
+  // timeline
+  /* 
+  TODO : Réduire l'anim à 2 secondes max
+  */
+  const tl = gsap.timeline();
+
+  tl.to(main, {
+    opacity: 0,
+    duration: 0.5,
+    ease: 'power2.inOut',
+  })
+    .to(logo, {
+      scale: scaleAmount,
+      duration: 1,
+      ease: 'power2.inOut',
+    })
+    .to(
+      transitionWrapper,
+      {
+        display: 'flex',
+        opacity: 1,
+        duration: 0.25,
+        ease: 'power2.inOut',
+      },
+      '-=0.5'
+    )
+    .fromTo(
+      transitionText,
+      {
+        display: 'flex',
+        opacity: 0,
+        y: -3 * 16,
+      },
+      {
+        opacity: 1,
+        duration: 0.25,
+        ease: 'power2.inOut',
+        y: 0,
+      },
+      '-=0.5'
+    )
+    .to(transitionWrapper, {
+      opacity: 1,
+      duration: 0.5,
+      ease: 'power2.inOut',
+      onComplete: () => {
+        gsap.to(transitionWrapper, {
+          display: 'none',
+        });
+      },
+    })
+    .to(logo, {
+      scale: 1,
+      duration: 1,
+      ease: 'power2.inOut',
+    });
+};
+
+/* 
+TODO: Remove when ok
+*/
+export const initBarbaClick = () => {
+  const triggers = document.querySelectorAll('[barba=trigger]');
+  triggers.forEach((trigger) => {
+    trigger.addEventListener('click', () => {
+      alert('fonctionne');
+    });
+  });
+};
