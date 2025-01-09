fec8553d8dd4e75708428cd83b7b42f7b98de58f
Clément Murzeau <clement@studiorelief.fr>
2025-01-09 18:49:10 +0100

    new barba transition

--- src/utils/global/gsapTransitionV2.ts
+++ src/utils/global/gsapTransitionV2.ts
@@ -1,11 +1,52 @@
-// import gsap from 'gsap';
+import { data } from '@barba/core';
+import gsap from 'gsap';
 
-export const gsapTransitionV2Out = () => {
-  //   const logo = document.querySelector('.navbar_logo-round');
+export function gsapTransitionV2() {
+  // const logo = document.querySelector('.navbar_logo-round');
   //   const logoMain = document.querySelector('.navbar_logo-text');
   //   const main = document.querySelector('.main-wrapper');
-  //   const transitionWrapper = document.querySelector('.transition_component');
+  const transitionWrapperV2 = document.querySelector('.transition-v2_component');
   //   const transitionText = document.querySelector('.transition_text');
   //   const viewportHeight = window.innerHeight;
   //   const scaleAmount = viewportHeight;
-};
+
+  const tl = gsap.timeline({
+    defaults: {
+      duration: 0.5,
+      ease: 'power2.inOut',
+    },
+  });
+
+  tl.fromTo(
+    transitionWrapperV2,
+    {
+      display: 'none',
+      //   marginTop: '100vh',
+      opacity: 0,
+      //   duration: 0.25,
+    },
+    {
+      display: 'flex',
+      //   marginTop: '0vh',
+      opacity: 1,
+      duration: 0.25,
+      ease: 'power1.out',
+    }
+  ).to(transitionWrapperV2, {
+    // marginTop: '-100vh',
+    opacity: 0,
+    duration: 0.25,
+    delay: 0.5,
+    ease: 'power1.in',
+  });
+
+  const AnimationPageTextV2 = () => {
+    const dataNamespace = data.next.namespace;
+    console.log(dataNamespace);
+    const transitionText = document.querySelector('.transition-v2_text');
+    if (transitionText) {
+      transitionText.textContent = dataNamespace;
+    }
+  };
+  AnimationPageTextV2();
+}
