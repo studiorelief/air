feda43453d3184488a708830b0070e8c2c7a4907
Clément Murzeau <clement@studiorelief.fr>
2025-01-09 10:27:32 +0100

    Release 09.01

--- src/utils/global/marker.ts
+++ src/utils/global/marker.ts
@@ -1,6 +1,9 @@
 import markerSDK from '@marker.io/browser';
 export async function initMarker() {
-  await markerSDK.loadWidget({
-    project: '6777e87de38da678c59baae6',
-  });
+  // Only load marker if URL contains 'webflow'
+  if (window.location.href.includes('webflow')) {
+    await markerSDK.loadWidget({
+      project: '6777e87de38da678c59baae6',
+    });
+  }
 }
